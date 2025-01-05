036f42e7566d507cc6b8f9c0f068864c450c4318
Angelosewase <sewasejo8@gmail.com>
2025-01-05 12:18:36 +0200

    init

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-lg text-red-200">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
